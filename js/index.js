429e4056d0f2a492e2269882e485049298781031
topoor <1239323088@qq.com>
2021-10-08 21:45:36 +0800

    增加了搜索框文字效果

--- js/index.js
+++ js/index.js
@@ -14,4 +14,26 @@ $(function() {
             location.href = './record/load.html';
         }
     })
+
+    // 搜索框文字动画效果
+    let mytext = $('.search input').prop('placeholder');
+    let str = '';
+    let timer;
+    let i = 0;
+    const addChar = () => {
+        console.log(str);
+        $('.search input').prop('placeholder', str + '_');
+        str += mytext.charAt(i++);
+        type();
+    }
+    const type = () => {
+        if (i < mytext.length+1){
+            timer = setTimeout(addChar, 100);
+        } else {
+            clearTimeout(timer);
+        }
+    }
+    $(document).ready(type);
+
 })
+
